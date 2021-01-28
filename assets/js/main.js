f92dbd212723faba85a5d1f0f1d782d40c3cd90b
val8119 <48407846+val8119@users.noreply.github.com>
2021-01-28 16:07:07 +0200

    Created base files

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// Selectors
+let inputPrincipal = document.querySelector(".principal");
+let inputInterest = document.querySelector(".interest");
+let inputDays = document.querySelector(".days");
+
+console.log(inputPrincipal.value, inputInterest.value, inputDays.value);
