a74ca7079ba1297d24c807dac2f4db8d8fc4475d
val8119 <48407846+val8119@users.noreply.github.com>
2021-01-28 16:57:18 +0200

    Added man functionality, code still shitty

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-// Selectors
-let inputPrincipal = document.querySelector(".principal");
-let inputInterest = document.querySelector(".interest");
-let inputDays = document.querySelector(".days");
-
-console.log(inputPrincipal.value, inputInterest.value, inputDays.value);
